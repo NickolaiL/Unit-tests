299cf94f6e54ecc387497464511c71d43a65b4a7
NickolaiL <nixon2234392@gmail.com>
2020-01-07 20:56:34 +0300

    EmployeeStorage Refactoring

--- TestNinja/Mocking/EmployeeController.cs
+++ TestNinja/Mocking/EmployeeController.cs
@@ -5,17 +5,16 @@ namespace TestNinja.Mocking
     public class EmployeeController
     {
         private EmployeeContext _db;
+        private IEmployeeStorage _employeeStorage;
 
-        public EmployeeController()
+        public EmployeeController(IEmployeeStorage employeeStorage = null)
         {
-            _db = new EmployeeContext();
+            _employeeStorage = employeeStorage ?? new EmployeeStorage();
         }
 
         public ActionResult DeleteEmployee(int id)
         {
-            var employee = _db.Employees.Find(id);
-            _db.Employees.Remove(employee);
-            _db.SaveChanges();
+            _employeeStorage.DeleteEmployee(id);
             return RedirectToAction("Employees");
         }
 
