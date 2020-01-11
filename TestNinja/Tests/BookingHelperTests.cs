5601fcfb8bf84358d8028badbd52d5ce039697e2
NickolaiL <nixon2234392@gmail.com>
2020-01-11 16:48:00 +0300

    Refactored BookingHelper class with parameter Dependency injection

---
+++ TestNinja/Tests/BookingHelperTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using TestNinja.Mocking;
+
+namespace TestNinja.Tests
+{
+    [TestFixture]
+    class BookingHelperOverlappingBookingsExistTests
+    {
+        [Test]
+        public void BookingStartsAndFinishesBeforeAnExistingBooking_ReturnEmptyString()
+        {
+
+        }
+    }
+}
